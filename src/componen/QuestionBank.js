2e411023a2495bec09fc4817e765b6d31312a41c
Haris-crypto <85698594+Haris-crypto@users.noreply.github.com>
2024-09-12 07:33:29 +0530

    first commit

---
+++ src/componen/QuestionBank.js
@@ -0,0 +1,40 @@
+const qBank = [
+    {
+        id: 1,
+        question: "What is the capital of Haryana?",
+        options: ["Yamunanagar", "Panipat", "Gurgaon", "Chandigarh"],
+        answer: "Chandigarh",
+    },
+    {
+        id: 2,
+        question: "Which is not belong to oops properties?",
+        options: ["inheritance", "polymorphism", "abstraction", "collision"],
+        answer: "collision",
+    },
+    {
+        id: 3,
+        question: "what keyword is used to check wheather a given property is valid or not?",
+        options: ["in", "is in", "exists", "lies"],
+        answer: "in"
+    },
+    {
+        id: 4,
+        question: "What is the capital of Uttarakhad?",
+        options: ["Roorkee", "Haridwar", "Dehradun", "Nanital"],
+        answer: "Dehradun"
+    },
+    {
+        id: 5,
+        question: "What is capital of Uttar Pradesh?",
+        options: ["GB Nagar", "Lucknow", "Prayagraj", "Agra"],
+        answer: "Lucknow"
+    },
+    {
+        id: 6,
+        question: "Who is prime minister of india?",
+        options: ["manmohan singh", "nitish kumar", "yogi", "narendra modi"],
+        answer: "narendra modi"
+    },
+]
+
+export default qBank;
